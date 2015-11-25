9e684cdc72b3423dbfc504f1e82c1c8833ebe751
MSP-Greg <Greg.mpls@gmail.com>
2015-11-25 13:54:45 -0600

    Updates to md code

---
+++ lib/su_info/Constant_Template_Code.rb
@@ -0,0 +1,167 @@
+module CTCode
+
+# load "D:/rb/lib/su_info/constant_template_code.rb"
+
+#{ face_1
+def CTCode.face_1()
+  # must have a model open with at least one face!
+  cns = Sketchup::Face
+  face = Sketchup.active_model.entities.grep(cns)[0]
+  t = ''
+  if (face)
+    pt_location = face.classify_point(ORIGIN)
+    t = case pt_location
+      when cns::PointInside                     then 'pt is inside'
+      when cns::PointNotOnPlane                 then 'pt not on plane'
+      when cns::PointOnEdge, cns::PointOnVertex then 'pt on perimeter'
+      when cns::PointOutside                    then 'pt is outside'
+      when cns::PointUnknown                    then 'pt error?'
+      else 'not trapped by case statement'
+    end
+  else
+    t = 'no face found'
+  end
+  puts t
+end
+#} face_1
+
+#{ len_1
+def CTCode.len_1()
+  cns = Length
+  h_units  = Hash.new('Unit Unknown')
+  h_format = Hash.new('Format Unknown')
+
+  h_units[cns::Centimeter] = 'cm'
+  h_units[cns::Feet]       = 'ft'
+  h_units[cns::Inches]     = 'in'
+  h_units[cns::Meter]      = 'm'
+  h_units[cns::Millimeter] = 'mm'
+
+  h_format[cns::Architectural] = 'Architectural'
+  h_format[cns::Decimal]       = 'Decimal'
+  h_format[cns::Engineering]   = 'Engineering'
+  h_format[cns::Fractional]    = 'Fractional'
+
+  om = Sketchup.active_model.options # OptionManager
+  op = om['UnitsOptions']            # OptionsProvider
+  units =  h_units[  op['LengthUnit']   ]
+  format = h_format[ op['LengthFormat'] ]
+  puts "Current model units are #{units}"
+  puts "Current model format is #{format}"
+end
+#} len_1
+
+#{ ro_1
+def CTCode.ro_1()
+  cns = Sketchup::RenderingOptions
+
+  # hash @@roc = RenderingOptions constants
+  #   key is constant value, value is constant name
+  @@roc = Hash.new("** No Constant! **")
+  # get all the constants, parse names, add to hash
+  cns.constants.each { |c|
+    text = c.to_s.dup
+    if    ( text.match(/^ROPSet/) ) ; text.sub!(/^ROPSet/, 'ROPSet  ')
+    elsif ( text.match(/^ROP/) )    ; text.sub!(/^ROP/   , 'ROP     ')
+    end
+    @@roc[cns.const_get(c)] = text
+  }
+
+  # dump hash to console
+  @@roc.sort.each { |r| puts "#{r[0].to_s.rjust(3)}  #{r[1]}" }
+
+  # create a RenderingOptionsObserver instance & create callback method
+  @@obs_ro = Sketchup::RenderingOptionsObserver.new
+  @@obs_ro = Object.new
+  def @@obs_ro.onRenderingOptionsChanged(ro, type)
+    puts "#{type.to_s.rjust(3)}  #{@@roc[type]}"
+  end
+  # attach the observer
+  Sketchup.active_model.rendering_options.add_observer(@@obs_ro)
+end
+#} ro_1
+
+
+#{ ro_2
+def CTCode.ro_2()
+  # create an observer
+  obs_ro = Sketchup::RenderingOptionsObserver.new
+  # add callback method
+  def obs_ro.onRenderingOptionsChanged(ro, type)
+    cns = ro.class
+    suffix = case type
+      when cns::ROPDrawHidden                  then 'DrawHidden'
+      when cns::ROPSetDisplayColorByLayer      then 'DisplayColorByLayer'
+      when cns::ROPSetDisplaySketchAxes        then 'DisplaySketchAxes'
+      when cns::ROPSetHideConstructionGeometry then 'HideConstructionGeometry'
+      when cns::ROPSetModelTransparency        then 'ModelTransparency'
+      when cns::ROPSetRenderMode               then 'RenderMode'
+      when cns::ROPSetSectionDisplayMode       then 'SectionDisplayMode'
+      when cns::ROPSetTexture                  then 'Texture'
+      else "Not caught by case statement"
+      end
+    puts suffix
+  end
+
+  # attach it to the Rendering_options of the model
+  Sketchup.active_model.rendering_options.add_observer(obs_ro)
+end
+#} ro_2
+
+# load "D:/rb/lib/su_info/constant_template_code.rb"
+
+#{ tool_1
+def CTCode.tool_1()
+  tool = Object.new
+  def tool.onLButtonDown(flags, x, y, view) ; CTCode.mouse(flags, x, y, view, 'Down') ; end
+  def tool.onMButtonDown(flags, x, y, view) ; CTCode.mouse(flags, x, y, view, 'Down') ; end
+  def tool.onRButtonDown(flags, x, y, view) ; CTCode.mouse(flags, x, y, view, 'Down') ; end
+
+  def tool.onLButtonUp(flags, x, y, view) ; CTCode.mouse(flags, x, y, view, 'Up') ; end
+  def tool.onMButtonUp(flags, x, y, view) ; CTCode.mouse(flags, x, y, view, 'Up') ; end
+  def tool.onRButtonUp(flags, x, y, view) ; CTCode.mouse(flags, x, y, view, 'Up') ; end
+
+  def tool.onLButtonDoubleClick(flags, x, y, view) ; CTCode.mouse(flags, x, y, view, 'DblClk') ; end
+  def tool.onMButtonDoubleClick(flags, x, y, view) ; CTCode.mouse(flags, x, y, view, 'DblClk') ; end
+  def tool.onRButtonDoubleClick(flags, x, y, view) ; CTCode.mouse(flags, x, y, view, 'DblClk') ; end
+  
+  
+  def tool.onKeyDown(key, repeat, flags, view)
+    t = flags.to_s(2).rjust(16)
+    tBin = "#{t[-16,4]} #{ t[-12,4]} #{ t[-8,4]} #{t[-4,4]}".rjust(18)
+#    puts "#{key.to_s.ljust(4)}\t#{flags.to_s.ljust(5)}\t#{tBin}"
+    alt =  (ALT_MODIFIER_MASK & key != 0).to_s.ljust(5)
+    cons = (CONSTRAIN_MODIFIER_MASK & key != 0).to_s.ljust(5)
+    copy = (COPY_MODIFIER_MASK & key != 0).to_s.ljust(5)
+    puts "alt = #{alt}   cons = #{cons}   copy = #{copy}"
+  end
+  Sketchup.active_model.select_tool(tool)
+end
+#} tool_1
+
+def CTCode.ro_1stub()
+  cnsl = '-----------------------------------------'
+  puts cnsl + '  CTCode.ro_1()'
+  CTCode.ro_1()
+  console
+end
+
+#{ mouse
+def CTCode.mouse(flags, x, y, view, up_down_dbl)
+  button  = []
+  key_mod = []
+  if (MK_LBUTTON & flags != 0) then button.push 'Left'   end
+  if (MK_MBUTTON & flags != 0) then button.push 'Middle' end
+  if (MK_RBUTTON & flags != 0) then button.push 'Right'  end
+  
+  if (MK_SHIFT   & flags != 0) then key_mod.push 'Shift' end
+  if (MK_CONTROL & flags != 0) then key_mod.push 'Ctrl'  end
+  if (MK_ALT     & flags != 0) then key_mod.push 'Alt'   end
+  s1 = up_down_dbl.ljust(7)
+  s2 = button.join(", ").ljust(20)
+  puts "Mouse Button #{s1} button = #{s2} keys = #{key_mod.join(", ")}" 
+end
+#} mouse
+# load "D:/rb/lib/su_info/constant_template_code.rb"
+end
+CTCode.tool_1()
--- lib/su_info/find_su_constants.rb
+++ lib/su_info/find_su_constants.rb
@@ -1,24 +1,35 @@
-# FindSUConstants createss two files, one in columns, the other tab delimited.
+# FindSUConstants createss three files, one in columns, another exactly
+# the same, but tab delimited, and the third for use in creating the
+# Sketchup_Constants.md file.
+
 # The files list all classes, modules, and constants defined by SketchUp.
 #
 # File layout is --
 # * Root (or un-namespaced) constants defined by the SketchUp environment
-# * Constants defined in SketchUp, starting with the SketchUp namespace
-# * List of SketchUp defined modules / classes that have no constants defined
+# * 'Namespaced' constants defined in SketchUp, starting with SketchUp
+# * List of SketchUp modules / classes that have no constants defined
 #
-# This module requires that the file 'native_ruby_cnsts.txt' exists in the directory 
-# defined as <code>ENV['TMP'] || ENV['TEMP']</code>.
+# This module requires that the file 'native_ruby_cnsts.txt' exists in the
+# directory defined as <code>ENV['TMP'] || ENV['TEMP']</code>.
 #
 # Lastly, remember to unload all of your extensions / plug-ins before runnning this.
 #
 module FindSUConstants
   # version number, listed in file headers
-  @@version = 1.1
-  
-  # text of columnar formatted file
+  @@version = 1.2
+
+  # cur dir
+  @@dir = File.dirname(__FILE__)
+  Dir.chdir(@@dir)
+
+  # intermediate text for md file
   @@text         = ''
+  
   # text of tab delimited file
   @@text_tab     = ''
+  # text of tab delimited file used for creating the Sketchup_Constants.md file
+  @@text_tab_md  = ''
+  
   # text of file section that listed 'no constants' objects, columnar formatted file
   @@no_const     = ''
   # text of file section that listed 'no constants' objects, tab delimited file
@@ -42,7 +53,12 @@ module FindSUConstants
   
   # used in file names, which are also shown in the 'finished' messagebox
   @@su_major = Sketchup.version.split('.')[0].rjust(2, '0')
-
+  if ( @@su_major.to_i >= 10 )
+    @@su_major_full = "20#{@@su_major}"
+  else
+    @@su_major_full = @@su_major.to_i.to_s
+  end
+  
   # Main entry point
   # @return [Void]
   def self._run()
@@ -52,38 +68,46 @@ module FindSUConstants
     objects = []
     constants = []
 
+    return unless (@@text_md   = self.file_get_str('Sketchup_Constants_List.md'))
+   
     # get hash from file of native Ruby objects
-    h_native = self.get_ruby_hash()
-
+    return unless self.get_ruby_hash(h_native)
     # sort root constants into two arrays, constants and module objects
     Object.constants.sort.each { |c|
+      o = Object.const_get(c)
       c_to_s = c.to_s
       next if ( /FindSUConstants/ =~ c_to_s || (h_native.key?(c_to_s) && !(re_include =~ c_to_s)) )
-      if ( eval(c_to_s).kind_of?(Module) )
-        objects << c_to_s
+      if ( o.kind_of?(Module) )
+#        objects << c
+        objects << o
       else
-        constants << c_to_s
+        constants << c
       end
     }
 
     # write root constants
     len_c = constants.length
     @@ctr_root += len_c
-    self.write_constants(constants, 0) if ( len_c > 0)
-
+    self.write_constants(constants, 0, Object) if ( len_c > 0)
+    @@text_md.sub!(/<< _gc_ >>/, @@text)
+    @@text = ''
+    
     # process root modules / classes
     if (objects.length > 0)
       # sort with Sketchup first
-      idx = objects.index('Sketchup')
-      objects[idx] = '      Sketchup'
-      objects.sort!
-      objects[0] = 'Sketchup'
+      objects.delete(Object)
+      objects.delete(Sketchup)
+      objects.sort! { |a,b| a.to_s <=> b.to_s }
+      objects.insert(0, Sketchup)
       # now find info on modules / classes
       objects.each { |o| self.find_nested_constants(o) }
     end
+    
+    @@text_md.sub!(/<< _nsc_ >>/, @@text)
+    @@text_md.sub!(/<< _noc_ >>/, @@no_const)
+    @@text = ''
 
     # write info on modules / classes with no constants
-    @@text     << "\n" << @@no_const
     @@text_tab << "\n" << @@no_const_tab
 
     # create header and add to text
@@ -91,10 +115,14 @@ module FindSUConstants
     @@text = hdr + @@text
     @@text_tab = hdr + @@text_tab
 
+    @@text_md.gsub!(/SketchUp_Constants_List/, "SketchUp #{@@su_major_full} Constants List")
+    
     # write files, show done message box
-    self.write_dev_file("su#{@@su_major}_constants.txt"    , @@text)
-    self.write_dev_file("su#{@@su_major}_constants_tab.txt", @@text_tab)
-    self.msg_box_done()
+    self.file_write("SketchUp_#{@@su_major}_Constants_List.md"    , @@text_md)
+    self.file_write("su#{@@su_major}_constants_tab.txt", @@text_tab)
+    self.file_write("su#{@@su_major}_constants_tab_md.txt", @@text_tab_md)
+#    self.msg_box_done()
+    load "#{@@dir}/create_su_constants_md.rb"
   end
 
   # Returns the file header
@@ -112,75 +140,96 @@ module FindSUConstants
   end
 
   # Finds all constants in object namespace, reentrant
-  # @param obj_name [String]
-	def self.find_nested_constants(obj_name)
-		constants = []
+  # @param obj [Object]
+	def self.find_nested_constants(obj)
+    constants = []
     objects   = []
     c_len = 0
     ctr = 0
-    eval("#{obj_name}.constants").each { |c|
-      o = eval("#{obj_name}::#{c}")
+    isCls = obj.kind_of?(Class)
+    obj.constants.each { |c|
+      next if (isCls && obj.superclass.const_defined?(c) )
+      o = obj.const_get(c)
       if ( o.kind_of?(Module) )
-        objects << c.to_s
+        objects << o
       else
-        constants << c.to_s
+        constants << c
       end
     }
-
     len_c = constants.length
     @@ctr_su += len_c
-#		s_class = eval(obj_name).kind_of?(Class) ? 'Class' : 'Module'
-    s_class = obj_name.kind_of?(Class) ? 'Class' : 'Module'
-
+    s_class = (obj.class == Class ? 'Class' : 'Module')
+    obj_str = obj.to_s
     if (len_c > 0)
-      t = "#{obj_name}::".ljust(@@width_1) + @@space_2 + s_class
-			@@text <<     "\n#{t}\n"
-      @@text_tab << "\n#{obj_name}::\t\t#{s_class}\n"
-      self.write_constants(constants, 2, obj_name)
-      @@text << "\n"
+			@@text << "<tr><td>#{obj_str}</td><td></td><td>#{s_class}</td></tr>\n"
+      @@text_tab << "\n#{obj_str}::\t\t#{s_class}\n"
+      self.write_constants(constants, 2, obj)
+      @@text << "<tr><td>&#160;</td>&#160;<td></td><td>&#160;</td></tr>\n"
       @@text_tab << "\n"
 		else
-      s_class = eval(obj_name).class == Class ? 'Class' : 'Module'
-      @@no_const     << "#{obj_name.ljust(@@width_1)}#{@@no_constants}#{s_class}\n"
-      @@no_const_tab << "#{obj_name}\tno constants\t#{s_class}\n"
+      @@no_const     << "<tr><td>#{obj_str}</td><td>#{s_class}</td><td>&#160;</td></tr>\n"
+      @@no_const_tab << "#{obj_str}\tno constants\t#{s_class}\n"
       @@ctr_su_no += 1
       if (@@ctr_su_no % 5 == 0)
-        @@no_const     << "\n"
+        @@no_const     << "<tr><td>&#160;</td>&#160;<td></td><td>&#160;</td></tr>\n"
         @@no_const_tab << "\n"
       end
     end
 
     if (objects.length > 0)
-      objects.sort!
-      objects.each { |o| self.find_nested_constants("#{obj_name}::#{o.to_s}") }
+      objects.sort! { |a,b| a.to_s <=> b.to_s }
+      objects.each { |o| self.find_nested_constants(o) }
     end
 	end
 
   # Creates and returns a Hash from the items in the native_ruby_cnsts.txt file
   # @return [Hash]
-  def self.get_ruby_hash()
+  def self.get_ruby_hash(hash)
+    ok = false
     ruby_info = nil
-    temp_dir = ENV['TMP'] || ENV['TEMP']
-    if (File.directory?(temp_dir))
-      f_name = "#{temp_dir}#{File::SEPARATOR}native_ruby_cnsts.txt"
+    dir = "../../su_info_txt" || ENV['TMP'] || ENV['TEMP']
+    if File.directory?(dir)
+      f_name = "#{dir}#{File::SEPARATOR}native_ruby_cnsts.txt"
       if File.exists?(f_name)
         a_ruby = File.read(f_name).split(/[\r\n\f]+/)
+        a_ruby.each { |k| hash[k] = nil }
+        ok = true
       else
         UI.messagebox("File #{f_name} does not exist\n\nExiting!")
-        exit
+      end
+    else
+      UI.messagebox("Directory #{dir} does not exist\n\nExiting!")
+    end
+    ok
+  end
+
+  # Returns a file as a String, location is `ENV['TMP'] || ENV['TEMP']`
+  # @param file [String]
+  # @param path [String,nil]
+  # @return [String,nil]
+  #
+  def self.file_get_str(file, path = nil )
+  ret = nil
+  dir = path || @@dir ||ENV['TMP'] || ENV['TEMP']
+    if File.directory?(dir)
+      f_name = "#{dir}#{File::SEPARATOR}#{file}"
+      if File.exists?(f_name)
+        ret = File.read(f_name)
+      else
+        UI.messagebox("File #{f_name} does not exist\n\nExiting!")
       end
     else
       UI.messagebox("Directory #{f_name} does not exist\n\nExiting!")
-      exit
     end
-    Hash[ *((a_ruby.collect { |v| [v, nil] }).flatten) ]
+    ret
   end
 
   # Shows the 'finished' UI.messagebox
   def self.msg_box_done()
     msg = "Wrote files in folder #{ENV['TMP'] || ENV['TEMP']} --\n"
     msg << "   su#{@@su_major}_constants.txt\n"
-    msg << "   su#{@@su_major}_constants_tab.txt\n\n"
+    msg << "   su#{@@su_major}_constants_tab.txt\n"
+        msg << "   su#{@@su_major}_constants_tab_md.txt\n\n"
     msg << "Found --\n"
     msg <<  "   #{@@ctr_root.to_s.rjust(4)} Constants at object / root\n"
     msg <<    "   #{@@ctr_su.to_s.rjust(4)} Constants defined in Sketchup classes\n"
@@ -188,37 +237,49 @@ module FindSUConstants
     UI.messagebox(msg)
   end
 
-  # Writes an array of constants to the two output files
+  # Writes an array of constants to the three output files
   # @param constants [Array<String>]
   # @param indent [Integer] indent width for nested constants
-  # @param ns [String] namespace of constants array
-  def self.write_constants(constants, indent, ns = nil)
+  # @param object [Object] namespace/parent of constants array
+  def self.write_constants(constants, indent, object)
     s_indent = '              '.slice(0, indent)
     width_1 = @@width_1 - indent
     constants.sort!
     ctr = 0
     len_c = constants.length
+    if (object == Object)
+      fqn = ''
+    else
+      fqn = "#{object.to_s}::"
+    end
     constants.each { |c|
-      o = ns ? eval("#{ns}::#{c}") : eval("#{c}")
-      @@text << "#{s_indent}#{c.ljust(width_1)}#{o.to_s.ljust(@@width_2)}#{o.class}\n"
-      @@text_tab << "#{c}\t#{o}\t#{o.class}\n"
-#      @@text << "#{s_indent}#{c.to_s.ljust(width_1)}#{o.to_s.ljust(@@width_2)}#{o.class}\n"
-#      @@text_tab << "#{c.to_s}\t#{o}\t#{o.class}\n"
+      o = object.const_get(c)
+      c = c.to_s
+      val = o.to_s.dup
+      if (/RUBY_/ =~ c)
+        val.sub!(/ Y/, '<br/>Y')
+        val.sub!(/\) \[/, ')<br/>[')
+      end
+      @@text << "<tr><td>#{c}</td><td>#{val}</td><td>#{o.class}</td></tr>\n"
+      @@text_tab << "#{c}\t#{val}\t#{o.class}\n"
+      @@text_tab_md << "#{fqn}#{c}\t#{val}\t#{o.class}\n"
       ctr += 1
       if (ctr % 5 == 0 && ctr != len_c)
-        @@text << "\n"
+        @@text << "<tr><td>&#160;</td>&#160;<td></td><td>&#160;</td></tr>\n"
         @@text_tab << "\n"
       end
     }
   end
 
   # Writes output file
-  # @param name [String] name of output file with extention
+  # @param f_name [String] name of output file with extention
   # @param string [String] file contents
-  def self.write_dev_file(name, string)
-    temp = ENV['TMP'] || ENV['TEMP']
-    if (File.directory?(temp))
-      file = File.open("#{temp}#{File::SEPARATOR}#{name}", "w")
+  def self.file_write(f_name, string)
+    dir = "../../su_info_txt" || ENV['TMP'] || ENV['TEMP']
+    dir = "../../docs/" if (f_name.slice(-2,2) == 'md')
+    
+    if (File.directory?(dir))
+      file = File.open("#{dir}#{File::SEPARATOR}#{f_name}", "w")
       if (file)
         file.write(string)
         file.close
@@ -227,6 +288,7 @@ module FindSUConstants
   end
 
 end
-#run
-# load 'D:/rb/su_info/find_su_constants.rb'
+
+# load 'D:/rb/lib/su_info/find_su_constants.rb'
 FindSUConstants._run()
+CreateSUConstantsMD._run()
